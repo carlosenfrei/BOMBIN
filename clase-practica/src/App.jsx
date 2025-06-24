7c1e63686e956d4ffc28c993e6db7a4df7b9db56
carlosenfrei <carlosenfrei@gmail.com>
2025-06-24 18:14:45 -0300

    Se mejoro estilado de carrito y jsx

--- clase-practica/src/App.jsx
+++ clase-practica/src/App.jsx
@@ -2,32 +2,16 @@ import './App.css';
 import NavBar from "./componets/NavBar";
 import ListaContenedor from "./componets/ListaContenedor";
 import Contador from "./componets/Contador"
-import { useState } from "react";
+import FormImput from "./componets/FormImput";
+import { useState } from "react"; //lo escribo entre llaves para desestrucutrar y evitar escribir React.useState
 
 function App() {
   const [nombre, setNombre] = useState("");
   const [mostrarSaludo, setMostrarSaludo] = useState(false);
-
-  const manejarCambio = (e) => {
-    setNombre(e.target.value);
-  };
-
-  const manejarSubmit = (e) => {
-    e.preventDefault();
-    if (nombre.trim() !== "") {
-      setMostrarSaludo(true);
-    }
-  };
-
-
-
   return (
       <div className="app-container">
         <NavBar/>
-        <form onSubmit={manejarSubmit}>
-          <input type="text" placeholder="Ingresa tu nombre" value={nombre} onChange={manejarCambio}/>
-          <button type="submit">Ingresar</button>
-        </form>
+        <FormImput nombre={nombre} setNombre={setNombre} setMostrarSaludo={setMostrarSaludo} />
         <ListaContenedor saludo={mostrarSaludo ? `Bienvenid@ ${nombre} a la tienda virtual de Bombin`: "Bienvenid@ a la tienda virtual de Bombin"}/>
         {/*<ListaContenedor saludo = {`Bienvenid@ ${nombre} a la tienda virtual de Bombin`} />*/}
         {/*<ListaContenedor saludo = {"Bienvenid@ "+ saludo1 + " a la tienda virtual de Bombin"} />*/}
